88281e96bbbdc38808c35b5feea046f379a2bba2
Jinglin Feng <jennyklosever@gmail.com>
2017-05-31 11:24:14 -0400

    testing1

--- server.R
+++ server.R
@@ -2,12 +2,12 @@ library(shiny)
 library(ggplot2)  
 library(mdsr)
 shinyServer(function(input, output) {
-  output$SAT<-renderTable(SAT_2010[c(1,17,49,23,18,10,2,11,8,37),])
+  output$SAT<-renderTable(SAT_2010[c(20,21,30,38,39,5,23,49,16,27,34,42),])
   observeEvent(input$newplot,{
     
     output$plot<-renderPlot({
       
-      SAT_2010_new<-SAT_2010[c(1,17,49,23,18,10,2,11,8,37),]
+      SAT_2010_new<-SAT_2010[c(20,21,30,38,39,5,23,49,16,27,34,42),]
       SAT_2010_salary_plot<-ggplot(data=SAT_2010_new,aes(x=salary,y=total))+
         geom_point()+geom_smooth(method = "lm",se = F, color = "red", linetype = "dashed")+ylab("Average Total SAT Score")+
         xlab("Average Teacher Salary")+geom_text(aes(label=state), size=3)
@@ -20,7 +20,7 @@ shinyServer(function(input, output) {
     
     output$plot2<-renderPlot({
       
-      SAT_2010_plot2<-SAT_2010[c(1,17,49,23,18,10,2,11,8,37),]%>%
+      SAT_2010_plot2<-SAT_2010[c(20,21,30,38,39,5,23,49,16,27,34,42),]%>%
         mutate(SAT_grp=ifelse(sat_pct<=27, "Low", "High"))
       ggplot(data=SAT_2010_plot2,aes(salary,total, col=SAT_grp))+
         geom_point()+geom_smooth(method=lm, se=FALSE)+ylab("Average Total SAT Score")+

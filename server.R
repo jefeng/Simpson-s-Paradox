c523a43edd34a89b1986e7c2efcaf219e10f3614
Jinglin Feng <jennyklosever@gmail.com>
2017-06-09 13:43:49 -0400

    Add files via upload

--- server.R
+++ server.R
@@ -1,19 +1,16 @@
 library(shiny)
+
 library(ggplot2)  
 library(mdsr)
 shinyServer(function(input, output) {
-  output$SAT<-renderTable(SAT_2010[c(20,21,30,38,39,5,23,49,16,27,34,42),])
-  observeEvent(input$newplot,{
+  
+  output$SAT<-renderTable(
     
-    output$plot<-renderPlot({
-      
-      SAT_2010_new<-SAT_2010[c(20,21,30,38,39,5,23,49,16,27,34,42),]
-      SAT_2010_salary_plot<-ggplot(data=SAT_2010_new,aes(x=salary,y=total))+
-        geom_point()+geom_smooth(method = "lm",se = F, color = "red", linetype = "dashed")+ylab("Average Total SAT Score")+
-        xlab("Average Teacher Salary")+geom_text(aes(label=state), size=3)
-      SAT_2010_salary_plot
-      
-    },height=362,width = 488)
+    
+    SAT_2010[c(20,21,30,38,39,5,23,49,16,27,34,42), c(1,4,8,9)]
+  )
+  output$text1 <- renderText({ 
+    "Move the slider to see the Simpson's Paradox effect"
   })
   
   observeEvent(input$newplot2,{
@@ -22,11 +19,45 @@ shinyServer(function(input, output) {
       
       SAT_2010_plot2<-SAT_2010[c(20,21,30,38,39,5,23,49,16,27,34,42),]%>%
         mutate(SAT_grp=ifelse(sat_pct<=27, "Low", "High"))
-      ggplot(data=SAT_2010_plot2,aes(salary,total, col=SAT_grp))+
-        geom_point()+geom_smooth(method=lm, se=FALSE)+ylab("Average Total SAT Score")+
-        xlab("Average Teacher Salary")+geom_text(aes(label=state), size=3)+
-        geom_smooth(method = "lm",se = F, color = "black", linetype = "dashed",lwd=1.5)
-    },height=360,width = 557)
+      
+      integer=8714-input$integer
+      
+      low=which(SAT_2010_plot2$salary<=55051)
+      high=which(SAT_2010_plot2$salary>55051)
+      SAT_2010_plot2[low, ]$salary=SAT_2010_plot2[low, ]$salary+integer
+      SAT_2010_plot2[low, ]$total=SAT_2010_plot2[low, ]$total+(integer*-0.01417 )    
+      
+      SAT_2010_plot2[high, ]$salary=SAT_2010_plot2[high, ]$salary-integer
+      SAT_2010_plot2[high, ]$total=SAT_2010_plot2[high, ]$total-(integer*-0.01417 )
+      
+      
+      
+      # ggplot(data=SAT_2010_plot2,aes(salary,total, col=SAT_grp))+
+      #   geom_point()+geom_smooth(method=lm, se=FALSE)+ylab("Average Total SAT Score")+
+      #   xlab("Average Teacher Salary")+geom_text(aes(label=state), size=3)+
+      #   geom_smooth(method = "lm",se = F, color = "black", linetype = "dashed",lwd=1.5)
+      # 
+      # 
+      # ggplot(data=SAT_2010_plot2,aes(salary,total, col=SAT_grp))+
+      #   geom_point()+xlim(c(45111,72734))+ylim(c(1473,1781))+ylab("Average Total SAT Score")+
+      #   xlab("Average Teacher Salary")
+      
+      
+      p=ggplot(data=SAT_2010_plot2,aes(salary,total, col=SAT_grp))+geom_smooth(method='lm', se=F)+
+              labs(x="Teachers'Salaries", y="SAT Scores")+geom_point()
+       
+       p +geom_text(aes(label=state), size=3)  + geom_smooth(method = "lm",se = F, color = "black", linetype = "longdash",lwd=1.5)
+       
+    },height=410,width = 637)
+    
+ 
+  })
+  
+  output$text2 <- renderText({ 
+    HTML(paste("Low Group: States have SAT Participation Rate less than 27% (National Level)",
+    "High Group: States have SAT Participation Rate greater than 27% (National Level)",
+    "Delta: Distance between two center points of these two groups", 
+    "Black dahed line represents the aggregated effect of the blue and red lines", sep ="<br/>"))
   })
   
 }
--- ui.R
+++ ui.R
@@ -3,24 +3,25 @@ library(ggplot2)
 library(mdsr)
 ui<-fluidPage(
   titlePanel("Simpson's Paradox"),
+  h4("Relationship of SAT scores and Teachers' Salaries"),
   tableOutput("SAT"),
-  h4("Relationship between SAT Scores and Teachers' Salaries"),
+  div(style = "position:absolute;top: 4.5em;right:47em;",
+   actionButton("newplot2", h5("Click here first to show the plot"))),
   
-  fluidPage(
-    fluidRow(
-      selectInput("Relationship between SAT Scores and Teachers'Salaries", 
-                  "Choose the one you think is correct",
-                  choices = c("Positive Relationship", "Negative Relationship", "Independent"))
-      
-      
-    ),
-    fluidRow(
-      column( 1, offset = 0, actionButton("newplot","Check the result")),
-      column(1, offset = 7, actionButton("newplot2", "solution")),
-      br(),
-      column( 6, plotOutput("plot")),
-      column( 5, plotOutput("plot2"))
-    )
+  fluidRow(
+    div(style = "position:absolute;top: 10em;right:42em;",
+    textOutput("text1")),
     
-  )
+    div(style = "position:absolute;top: 13em;right:43em;",
+      sliderInput("integer", "Delta:",
+                  min=0, max=8714, value=8714)),
+    
+    div(style = "position:absolute;top: 19em;right:24em;",
+       plotOutput("plot2")),
+    
+    div(style = "position:absolute;top: 10em;right:2em;",
+    htmlOutput("text2"))
+    )
 )
+
+
